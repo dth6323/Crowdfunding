884681ee0876525404c63fa217ba19c3d6a9499a
dth6323 <dth6323@gmail.com>
2025-02-18 17:13:13 +0700

    constructor

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 contract Crowdfunding {
-     uint256 public  totalAmountFunded;
+    uint256 public  constant MINIMUM_FUND = 0.001 ether;
+    address public immutable i_owner;
+    constructor (){
+        i_owner = msg.sender;
+    }
     function fund() public payable {
         uint256 fundAmount = msg.value;
-        totalAmountFunded += fundAmount;
+        require(fundAmount >= MINIMUM_FUND, "No availabel amount");
         }
     function withdraw() public {
         
